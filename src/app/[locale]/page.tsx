a355fba7bfa7be615ed0433a04bc61f990f21ac4
Rummiya <rumibashirova@gmail.com>
2025-06-25 18:07:23 +0600

    chore(ui): add shadcn button for testing

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/shared/ui/button';
 import { useTranslations } from 'next-intl';
 
 export default function Home() {
 	const t = useTranslations('home');
 
-	return <div>{t('title')}</div>;
+	return (
+		<div>
+			<h1>{t('title')}</h1>
+			<Button variant={'outline'}>Зарегистрироваться</Button>
+		</div>
+	);
 }
