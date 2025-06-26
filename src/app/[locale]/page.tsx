ed59f67564989ee158aa76d1786c97377be5e1d1
Rummiya <rumibashirova@gmail.com>
2025-06-26 18:36:26 +0600

    refactor(ui): rewrite badge classes to project style

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,13 +1,15 @@
+import { Badge } from '@/shared/ui/badge';
 import { Button } from '@/shared/ui/button';
 import { useTranslations } from 'next-intl';
 
 export default function Home() {
-	const t = useTranslations('home');
+  const t = useTranslations('home');
 
-	return (
-		<div>
-			<h1 className='foreground'>{t('title')}</h1>
-			<Button variant={'outline'}>Зарегистрироваться</Button>
-		</div>
-	);
+  return (
+    <div>
+      <h1 className="foreground">{t('title')}</h1>
+      <Button variant={'outline'}>Зарегистрироваться</Button>
+      <Badge variant={'secondary'}>barber</Badge>
+    </div>
+  );
 }
